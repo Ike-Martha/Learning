4d44337bef326d00f2c98805ec0d781997c90024
Ike-Martha <amagwen36@gmail.com>
2024-04-20 17:24:41 +0100

    Creating decision logic with if statement

--- Array/Program.cs
+++ Array/Program.cs
@@ -13,8 +13,23 @@ namespace Application
             int roll3 = dice.Next(1, 7);
 
             int total = roll1 + roll2 + roll3;
-            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total} ");
-
+           Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total} ");
+            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+            {
+                Console.WriteLine("You rolled doubles! +2 bonus to total!");
+            }
+            if((roll1 == roll2) && (roll2 == roll3))
+            {
+                Console.WriteLine("You rolled trippled! +6 bonus to total");
+            }
+            if(total >= 15)
+            {
+                Console.WriteLine("You win");
+            }
+           if(total < 15) 
+            {
+                Console.WriteLine("Sorry, you lose");
+            }
 
 
         }
